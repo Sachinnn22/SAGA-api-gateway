996076ad1e8759169b3ea3d6428db0932a7fb7e1
Sachinnn22 <sachinduchirau2@gmail.com>
2026-08-27 17:16:10 +0530

    change file

--- src/main/java/com/example/apigateway/config/CorsConfig.java
+++ src/main/java/com/example/apigateway/config/CorsConfig.java
@@ -14,7 +14,9 @@ public class CorsConfig {
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        
+        corsConfig.setAllowedOriginPatterns(Arrays.asList("*"));
+        
         corsConfig.setMaxAge(3600L);
         corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         corsConfig.setAllowedHeaders(Arrays.asList("*"));
